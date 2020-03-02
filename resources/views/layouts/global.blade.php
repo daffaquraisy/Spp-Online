8f36b79bfc78f0223016ab60b2e380c5399c8f66
daffaquraisy <daffaquraisy@gmail.com>
2020-03-02 21:35:58 +0700

    nostalgic

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -51,7 +51,7 @@
   <li class="nav-item">
   <a class="nav-link" href="{{route('users.index')}}">
       <i class="fas fa-fw fa-users"></i>
-      <span>Manage Petugas</span></a>
+      <span>Manage Users</span></a>
   </li>
 
   <li class="nav-item">
